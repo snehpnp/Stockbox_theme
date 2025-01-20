7e2161b356a441b895a58a40f16b1d7d944d48db
ajay90989 <ajay.office.patidar>
2025-01-20 14:50:13 +0530

    done

--- backend/App/controllers/Pushnotification.js
+++ backend/App/controllers/Pushnotification.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 // const serviceAccount = require('../../template/stockbox-15e55-firebase-adminsdk-1zz93-5e353b0a02.json');
 const serviceAccount = require(process.env.SERVICE_ACCOUNT_PATH);
-// Initialize Firebase Admin SDK
+
 if (!admin.apps.length) {
   console.log('Initializing Firebase Admin SDK...');
   admin.initializeApp({
@@ -11,17 +11,17 @@ if (!admin.apps.length) {
 }
 
 
-async function sendFCMNotification(title, body, tokens, type="") {
+async function sendFCMNotification(title, body, tokens, type = "") {
   try {
 
-   const tokenss = [...new Set(tokens)];
+    const tokenss = [...new Set(tokens)];
 
 
-   const uniqueValidTokens = tokenss
-   .filter((token) => token && token.trim() !== '') // Remove empty tokens
-   .filter((token, index, self) => self.indexOf(token) === index); // Remove duplicates
+    const uniqueValidTokens = tokenss
+      .filter((token) => token && token.trim() !== '') // Remove empty tokens
+      .filter((token, index, self) => self.indexOf(token) === index); // Remove duplicates
 
-       const messages = uniqueValidTokens.map(token => ({
+    const messages = uniqueValidTokens.map(token => ({
       token: token,
       notification: {
         title: title,
@@ -29,18 +29,18 @@ async function sendFCMNotification(title, body, tokens, type="") {
       },
       data: {
         additional_data: 'value',
-        type: type, 
+        type: type,
       },
       android: {
         priority: "high", // High priority for real-time delivery
         ttl: 3600000, // Time-to-live: 1 hour in milliseconds
       },
-      
+
     }));
 
 
 
-    
+
     const response = await Promise.all(
       messages.map(message => admin.messaging().send(message))
     );
