818e71643baf423b7608c7447269ddb340e1f51a
ajay90989 <ajay.office.patidar>
2025-01-23 11:31:53 +0530

    done

--- Frontend/src/App/components/Navbars/Navbar.js
+++ Frontend/src/App/components/Navbars/Navbar.js
@@ -3,9 +3,10 @@ import Logo from "../Images/LOGO.png";
 import ProfileImage from "../Images/logo1.png";
 import { FaBell, FaBars } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
-import { ReadNotificationStatus, getDashboardNotification, GetAllNotificationRead } from '../../Services/Admin/Admin'
+import { ReadNotificationStatus, getDashboardNotification, GetAllNotificationRead, gettradestatus, basicsettinglist, UpdateLogin_status } from '../../Services/Admin/Admin'
 import Swal from 'sweetalert2';
 import { formatDistanceToNow } from 'date-fns';
+import { image_baseurl } from "../../../Utils/config";
 
 
 const Navbar = ({ headerStatus, toggleHeaderStatus }) => {
@@ -13,7 +14,11 @@ const Navbar = ({ headerStatus, toggleHeaderStatus }) => {
 
   useEffect(() => {
     getdemoclient()
+    gettradedetail();
   }, [])
+
+
+
 
   const navigate = useNavigate();
 
@@ -28,6 +33,13 @@ const Navbar = ({ headerStatus, toggleHeaderStatus }) => {
   const [getstatus, setGetstatus] = useState([]);
   const [badgecount, setBadgecount] = useState([]);
 
+  const [statusinfo, setStatusinfo] = useState({
+    aliceuserid: "",
+    apikey: "",
+    secretkey: ""
+  });
+
+
 
 
 
@@ -39,9 +51,6 @@ const Navbar = ({ headerStatus, toggleHeaderStatus }) => {
       window.location.href = "/login";
     }
   };
-
-
-
 
 
 
@@ -131,6 +140,138 @@ const Navbar = ({ headerStatus, toggleHeaderStatus }) => {
 
 
 
+  const gettradedetail = async () => {
+    try {
+      const response = await basicsettinglist(token);
+      if (response?.status) {
+        const data = response.data?.[0];
+        if (data) {
+          setGetstatus(response.data);
+
+          const faviconElement = document.querySelector("link[rel='icon']");
+          if (faviconElement) {
+            faviconElement.href = `${image_baseurl}uploads/basicsetting/${data.favicon}`;
+          } else {
+            console.warn("Favicon element not found");
+          }
+
+
+          const companyNameElement = document.querySelector(".companyName");
+          if (companyNameElement) {
+            companyNameElement.textContent = data.from_name;
+          }
+          if (data.staffstatus === 0) {
+            localStorage.clear();
+          }
+        }
+      } else {
+        console.error("Invalid response status:", response);
+      }
+    } catch (error) {
+      console.error("Error fetching services:", error);
+    }
+  };
+
+
+
+
+
+  const getstatusdetaile = async () => {
+    if (!statusinfo.aliceuserid || !statusinfo.apikey || !statusinfo.secretkey) {
+      Swal.fire({
+        title: 'Warning!',
+        text: "Please fill in all fields",
+        icon: 'warning',
+        confirmButtonText: 'OK',
+        timer: 2000,
+      });
+      return;
+    }
+    const data = {
+      aliceuserid: statusinfo.aliceuserid || getstatus[0].aliceuserid,
+      apikey: statusinfo.apikey || getstatus[0].apikey,
+      secretkey: statusinfo.secretkey || getstatus[0].secretkey
+    };
+    try {
+      const response = await gettradestatus(data, token);
+      if (response.status === true && response.url) {
+        window.location.href = response.url;
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+
+
+
+
+  const UpdateloginOff = async (e) => {
+    const dataoff = e.target.checked ? 1 : 0;
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "Do you really want to log off?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, log off',
+      cancelButtonText: 'Cancel',
+      reverseButtons: true
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const data = {
+            brokerloginstatus: dataoff,
+          };
+          const response = await UpdateLogin_status(data, token);
+          if (response?.status) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Successful!',
+              text: 'Status Log Out Successful',
+              timer: 1500,
+              timerProgressBar: true,
+            });
+          }
+        } catch (error) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Update Failed',
+            text: 'There was an error logging out',
+            timer: 1500,
+            timerProgressBar: true,
+          });
+        }
+      }
+    });
+  };
+
+
+
+  const handleToggle = () => {
+    if (getstatus[0]?.brokerloginstatus === 1) {
+      setIsChecked(!isChecked);
+
+    } else {
+      setModel(true);
+      setStatusinfo({
+        aliceuserid: getstatus[0]?.aliceuserid || "",
+        apikey: getstatus[0]?.apikey || "",
+        secretkey: getstatus[0]?.secretkey || ""
+      });
+    }
+  };
+
+
+
+
+  useEffect(() => {
+    if (getstatus[0]?.brokerloginstatus === 1) {
+      setIsChecked(true);
+    }
+  }, [getstatus]);
+
+
+
   return (
     <>
 
@@ -193,20 +334,30 @@ const Navbar = ({ headerStatus, toggleHeaderStatus }) => {
             </div>
             <div className="col-7">
               <div className="d-flex align-items-center position-relative justify-content-end">
-                <div className="d-sm-flex">
+                <div className='d-flex'>
                   <span className="switch-label p-1">
                     Trading Status:
+                    <span style={{ color: isChecked ? 'green' : 'red' }}>
+                      {isChecked ? "On" : "Off"}
+                    </span>
                   </span>
-                  <div
-                    className="form-check form-switch form-check-dark"
-                    style={{ margin: "inherit", fontSize: 21 }}
-                  >
-                    <span style={{ color: "red", fontSize: 16 }}>Off</span>
+
+                  <div className="form-check form-switch form-check-dark" style={{ margin: "inherit", fontSize: "21px" }}>
                     <input
                       className="form-check-input"
                       type="checkbox"
                       role="switch"
                       id="flexSwitchCheckDark"
+                      disabled={isDisabled}
+                      checked={isChecked}
+                      onChange={(e) => {
+                        if (e.target.checked) {
+                          handleToggle();
+                        } else {
+                          setIsDisabled(true);
+                          // UpdateloginOff(e);
+                        }
+                      }}
                     />
                   </div>
                 </div>
@@ -476,10 +627,77 @@ const Navbar = ({ headerStatus, toggleHeaderStatus }) => {
               </div>
             </div>
           </div>
-
-
-
         </div>
+        {model && (
+          <>
+            <div className="modal-backdrop fade show"></div>
+            <div
+              className="modal fade show"
+              style={{ display: 'block' }}
+              tabIndex={-1}
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      Trading Status
+                    </h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      onClick={() => setModel(false)}
+                    />
+                  </div>
+                  <div className="modal-body">
+                    <form>
+                      <label> User ID </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={statusinfo.aliceuserid}
+                        onChange={(e) => setStatusinfo({ ...statusinfo, aliceuserid: e.target.value })}
+                      />
+                      <label> API Key </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={statusinfo.apikey}
+                        onChange={(e) => setStatusinfo({ ...statusinfo, apikey: e.target.value })}
+                      />
+                      <label> Secret Key </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={statusinfo.secretkey}
+                        onChange={(e) => setStatusinfo({ ...statusinfo, secretkey: e.target.value })}
+                      />
+                    </form>
+
+
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() => setModel(false)}
+                    >
+                      Close
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      onClick={getstatusdetaile}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </nav>
     </>
   );
