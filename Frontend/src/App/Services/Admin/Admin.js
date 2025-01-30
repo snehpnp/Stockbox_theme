1fd8b0bc4ffab6c16cd9c54123c1cecc92bffb7e
ajay90989 <ajay.office.patidar>
2025-01-30 14:44:44 +0530

    done

--- Frontend/src/App/Services/Admin/Admin.js
+++ Frontend/src/App/Services/Admin/Admin.js
@@ -676,19 +676,42 @@ export async function getstaffperuser(_id, token) {
 // add basket 
 
 export async function Addbasketplan(data, token) {
+    const formData = new FormData();
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+    formData.append('full_price', data.full_price);
+    formData.append('basket_price', data.basket_price);
+    formData.append('mininvamount', data.mininvamount);
+    formData.append('themename', data.themename);
+    formData.append('cagr', data.cagr);
+    formData.append('frequency', data.frequency);
+    formData.append('validity', data.validity);
+    formData.append('next_rebalance_date', data.next_rebalance_date);
+    formData.append('type', data.type);
+    formData.append('add_by', data.add_by);
+    formData.append('short_description', data.short_description);
+    formData.append('image', data.image);
+    formData.append('rationale', data.rationale);
+    formData.append('methodology', data.methodology);
+
+
+
+
     try {
-        const res = await axios.post(`${Config.base_url}basket/add`, data, {
+        const res = await axios.post(`${Config.base_url}basket/add`, formData, {
             headers: {
-                data: {},
+                'Content-Type': 'multipart/form-data',
                 'Authorization': `${token}`,
             },
         });
-
         return res?.data;
     } catch (err) {
         return err.response?.data || err.message;
     }
 }
+
+
+
 
 export async function Addstockbasketform(data, token) {
     try {
@@ -735,20 +758,37 @@ export async function getStock(token) {
 // update basket 
 
 export async function Updatebasket(data, token) {
+
+    const formData = new FormData();
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+    formData.append('full_price', data.full_price);
+    formData.append('basket_price', data.basket_price);
+    formData.append('mininvamount', data.mininvamount);
+    formData.append('themename', data.themename);
+    formData.append('cagr', data.cagr);
+    formData.append('frequency', data.frequency);
+    formData.append('validity', data.validity);
+    formData.append('next_rebalance_date', data.next_rebalance_date);
+    formData.append('type', data.type);
+    formData.append('id', data.id);
+    formData.append('short_description', data.short_description);
+    formData.append('image', data.image);
+    formData.append('rationale', data.rationale);
+    formData.append('methodology', data.methodology);
+
     try {
-        const res = await axios.put(`${Config.base_url}basket/update`, data, {
+        const res = await axios.put(`${Config.base_url}basket/update`, formData, {
             headers: {
-                data: {},
+                'Content-Type': 'multipart/form-data',
                 'Authorization': `${token}`,
             },
         });
-
         return res?.data;
     } catch (err) {
         return err.response?.data || err.message;
     }
 }
-
 
 
 //  update stock detail
@@ -772,7 +812,7 @@ export async function updateStockList(data, token) {
 // plan list 
 
 
-export async function getplanlist(token) { 
+export async function getplanlist(token) {
 
     try {
         const res = await axios.get(`${Config.base_url}plan/list`, {
@@ -2189,7 +2229,7 @@ export async function getPayementhistorywithfilter(data, token) {
             },
         });
 
-        
+
         return res?.data;
     } catch (err) {
         return err;
