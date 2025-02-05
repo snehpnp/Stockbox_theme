f1e650b93081abafd822612d5d6950586c9d2a6f
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-02-05 12:21:17 +0530

    Refactor RebalanceStock component layout and enhance button functionality

--- Frontend/src/App/Layout/Users/Basket/RebalanceStock.jsx
+++ Frontend/src/App/Layout/Users/Basket/RebalanceStock.jsx
@@ -14,61 +14,84 @@ const RebalanceStock = () => {
       >
       <div className="card">
       <div className="card-body">
-        <div className="tab-content" id="pills-tabContent">
-          <div className="tab-pane fade" id="primary-pills-home" role="tabpanel">
-            <div className="d-flex align-items-center">
-              <input
-                type="text"
-                className="form-control w-50"
-                readOnly=""
-                defaultValue=""
-              />
-              <button className="btn btn-outline-secondary ms-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-copy"
-                >
-                  <rect x={9} y={9} width={13} height={13} rx={2} ry={2} />
-                  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
-                </svg>
-              </button>
-              <button className="btn btn-outline-secondary ms-2">
-                <i className="bx bx-share-alt" />
-              </button>
-            </div>
-          </div>
+        <div className='float-end mb-3'>
+        <button className='btn btn-primary'>View</button>
+         <button className='btn btn-danger ms-2'>Sell</button>
+         </div>
           <div
-            className="tab-pane fade active show"
-            id="primary-pills-profile"
-            role="tabpanel"
+            className=""
+           
           >
             <div className="table-responsive">
               <table className="table mb-0">
                 <thead className="table-primary">
                   <tr>
                     <th scope="col">Name</th>
-                    <th scope="col">Earning Amt.</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Date</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">CMP</th>
+                    <th scope="col">Weightage</th>
                   </tr>
                 </thead>
-                <tbody />
+                <tbody>
+                  <tr>
+                    <td>Reliance</td>
+                    <td>2000</td>
+                    <td>2100</td>
+                    <td>20%</td>
+                  </tr>
+                  <tr>
+                    <td>Reliance</td>
+                    <td>2000</td>
+                    <td>2100</td>
+                    <td>20%</td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           </div>
         </div>
       </div>
-    </div>
     
-            Rebalance stock
+      <div className="card mt-4">
+      <div className="card-body">
+        <div className='float-end mb-3'>
+        <button className='btn btn-primary'>View</button>
+         <button className='btn btn-success ms-2'>Buy</button>
+         </div>
+          <div
+            className=""
+           
+          >
+            <div className="table-responsive">
+              <table className="table mb-0">
+                <thead className="table-primary">
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">CMP</th>
+                    <th scope="col">Weightage</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Reliance</td>
+                    <td>2000</td>
+                    <td>2100</td>
+                    <td>20%</td>
+                  </tr>
+                  <tr>
+                    <td>Reliance</td>
+                    <td>2000</td>
+                    <td>2100</td>
+                    <td>20%</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+         
         </Content>
     )
 }
