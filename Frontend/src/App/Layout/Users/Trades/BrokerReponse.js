0bca81c09cbf93484b5876b6f2de82a0c0babc4e
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-02-06 10:38:39 +0530

    Add animated "No Data Found" SVG component to Blogs and News sections

--- Frontend/src/App/Layout/Users/Trades/BrokerReponse.js
+++ Frontend/src/App/Layout/Users/Trades/BrokerReponse.js
@@ -152,7 +152,8 @@ const BrokerResponse = () => {
                 </div>
               </div>
             </div>
-          )) :      <div className="dark text-center">
+          )) :    
+            <div className="dark text-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 240 160"
