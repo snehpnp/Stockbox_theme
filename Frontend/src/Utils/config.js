0f27035d7baef248112927fc99ef8119cf662d1e
Ashishprajapati2906 <121096675+Ashishprajapati2906@users.noreply.github.com>
2025-02-11 13:49:52 +0530

    done

--- Frontend/src/Utils/config.js
+++ Frontend/src/Utils/config.js
@@ -6,7 +6,7 @@ export const soket_url = `http://localhost:5001/`;
 export const image_baseurl = `https://stockboxpnp.pnpuniverse.com/`;
 
 
-
+ 
 
 // export const soket_url = `https://stockboxpnp.pnpuniverse.com:1001/`;
 // export const base_url = `${window.location.origin}/backend/`
