ea0ea66f3c31e6a2b3ed36ddbbd0322a47c1184e
ajay90989 <ajay.office.patidar>
2025-02-20 12:27:05 +0530

    done

--- Frontend/src/App/Layout/Admin/Coupon/Updatecoupon.jsx
+++ Frontend/src/App/Layout/Admin/Coupon/Updatecoupon.jsx
@@ -118,12 +118,12 @@ const Updatecoupon = () => {
       limitation: values.limitation,
       id: row._id,
     };
-    console.log("req data", req);
+    ;
 
 
     try {
       const response = await updateCouponbyadmin(req, token);
-      console.log("datata", response);
+
 
       if (response.status) {
         Swal.fire({
