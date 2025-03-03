3e304ba1f15ff36b1d2fbec6cbd095e350470bd5
ajay90989 <ajay.office.patidar@gmail.com>
2025-03-03 10:52:40 +0530

    done

--- Frontend/src/Utils/config.js
+++ Frontend/src/Utils/config.js
@@ -5,9 +5,9 @@ export const Doamin_url = `${window.location.origin}/`;
 
 
 
-// export const base_url = `http://localhost:5001/`;
-// export const soket_url = `http://localhost:5001/`;
-// export const image_baseurl = `https://stockboxpnp.pnpuniverse.com/`;
+export const base_url = `http://localhost:5001/`;
+export const soket_url = `http://localhost:5001/`;
+export const image_baseurl = `https://stockboxpnp.pnpuniverse.com/`;
 
 
 
@@ -15,6 +15,6 @@ export const Doamin_url = `${window.location.origin}/`;
 
 
 
-export const soket_url = `${window.location.origin}:1001/`;
-export const base_url = `${window.location.origin}/backend/`
-export const image_baseurl = `${window.location.origin}/`;
+// export const soket_url = `${window.location.origin}:1001/`;
+// export const base_url = `${window.location.origin}/backend/`
+// export const image_baseurl = `${window.location.origin}/`;
