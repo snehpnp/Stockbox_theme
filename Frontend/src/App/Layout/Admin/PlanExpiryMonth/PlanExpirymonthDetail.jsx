a484e4258eefe95a30f49f82e00667f3d054d74a
ajay90989 <ajay.office.patidar@gmail.com>
2025-03-08 15:31:42 +0530

    done

--- Frontend/src/App/Layout/Admin/PlanExpiryMonth/PlanExpirymonthDetail.jsx
+++ Frontend/src/App/Layout/Admin/PlanExpiryMonth/PlanExpirymonthDetail.jsx
@@ -83,26 +83,8 @@ const PlanExpirymonthDetail = () => {
     // };
 
 
-    const monthMap = {
-        "01": "January",
-        "02": "February",
-        "03": "March",
-        "04": "April",
-        "05": "May",
-        "06": "June",
-        "07": "July",
-        "08": "August",
-        "09": "September",
-        "10": "October",
-        "11": "November",
-        "12": "December"
-    };
-
     const monthCode = item?.month?.slice(0, 2);
     const year = item?.month?.slice(2)
-
-    console.log(monthCode);
-    console.log(year);
 
 
 
