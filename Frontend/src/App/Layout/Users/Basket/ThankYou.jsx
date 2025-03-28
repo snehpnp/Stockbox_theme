2c7ea4e949711a993142e17f476b444eb7c829bc
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-03-28 18:12:45 +0530

    Update ThankYou component: add image and change button text to 'Go Back'

--- Frontend/src/App/Layout/Users/Basket/ThankYou.jsx
+++ Frontend/src/App/Layout/Users/Basket/ThankYou.jsx
@@ -6,10 +6,11 @@ const ThankYou = () => {
         <div className="col-md-8 mx-auto mt-5">
     <div className="wrapper-1">
       <div className="wrapper-2">
+        <img style={{width:'100px'}} src='https://cdn-icons-png.flaticon.com/512/7816/7816094.png'/>
         <h1>Thank you !</h1>
         <p>You have successfully subscribed the basket.</p>
         
-        <button className="btn btn-primary mt-4">Go Home</button>
+        <button className="btn btn-primary mt-4">Go Back</button>
       </div>
     </div>
   </div>
