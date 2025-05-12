f36fd9d3057abb3416c3f5e0033e15224f2b8e43
ajay90989 <ajay.office.patidar@gmail.com>
2025-05-12 12:50:24 +0530

    done

--- Frontend/src/App/Layout/Admin/Plans/Addplan.jsx
+++ Frontend/src/App/Layout/Admin/Plans/Addplan.jsx
@@ -15,11 +15,12 @@ const Addplan = () => {
     const token = localStorage.getItem("token");
     const [clients, setClients] = useState([]);
     const [plan, setPlan] = useState([]);
+    const [isFreeTrialZero, setIsFreeTrialZero] = useState("");
 
     const [loading, setLoading] = useState(false);
 
 
- 
+
 
     const getcategoryplanlist = async () => {
         try {
@@ -81,14 +82,14 @@ const Addplan = () => {
 
 
             if (response.status) {
-                showCustomAlert('Success',"Create Successful!",navigate,"/admin/plan")
+                showCustomAlert('Success', "Create Successful!", navigate, "/admin/plan")
             } else {
-                showCustomAlert('error',response.message)
+                showCustomAlert('error', response.message)
                 setLoading(false)
             }
         } catch (error) {
             setLoading(false)
-            showCustomAlert('error',"An unexpected error occurred. Please try again later.")
+            showCustomAlert('error', "An unexpected error occurred. Please try again later.")
         }
     };
 
@@ -106,6 +107,18 @@ const Addplan = () => {
         onSubmit,
     });
 
+
+    useEffect(() => {
+        const matchedClient = clients.find(
+            (item) => item._id === formik.values.category
+        );
+
+        if (matchedClient && matchedClient.freetrial_status == 1) {
+            setIsFreeTrialZero(1);
+        } else {
+            setIsFreeTrialZero(0);
+        }
+    }, [formik.values.category, clients]);
 
 
 
@@ -132,14 +145,22 @@ const Addplan = () => {
             label_size: 12,
             col_size: 3,
             disable: false,
-            options: [
-                { value: "1 month", label: "1 Month" },
-                { value: "3 months", label: "3 Months" },
-                { value: "6 months", label: "6 Months" },
-                { value: "1 year", label: "1 Year" }
-            ].filter((option) => {
-                return !plan.some((item) => item?.validity === option.value);
-            }),
+            options: isFreeTrialZero == 0
+                ? [
+                    { value: "1 month", label: "1 Month" },
+                    { value: "3 months", label: "3 Months" },
+                    { value: "6 months", label: "6 Months" },
+                    { value: "1 year", label: "1 Year" }
+                ]
+                : [
+                    { value: "1 day", label: "1 day" },
+                    { value: "2 days", label: "2 days" },
+                    { value: "3 days", label: "3 days" },
+                    { value: "4 days", label: "4 days" },
+                    { value: "5 days", label: "5 days" },
+                    { value: "6 days", label: "6 days" },
+                    { value: "7 days", label: "7 days" }
+                ].filter(option => !plan.some(item => item?.validity === option.value)),
             star: true
         },
         {
@@ -198,16 +219,16 @@ const Addplan = () => {
 
     return (
         <Content
-       Page_title="Add New Package"
-       backbutton_status={true}
-       backbutton_title={"Back"}
+            Page_title="Add New Package"
+            backbutton_status={true}
+            backbutton_title={"Back"}
             button_status={false}
-             button_title="Add Package">
+            button_title="Add Package">
             <div >
                 <DynamicForm
                     fields={fields}
                     formik={formik}
-                    
+
                     btn_name="Add Package"
                     btn_name1="Cancel"
                     sumit_btn={true}
