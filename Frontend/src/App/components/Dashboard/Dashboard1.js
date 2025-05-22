020ec639236c866e830e5b0b6cc5cf5de044a0f0
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-05-22 10:35:09 +0530

    Refactor DashboardCards component to streamline card data structure and improve readability

--- Frontend/src/App/components/Dashboard/Dashboard1.js
+++ Frontend/src/App/components/Dashboard/Dashboard1.js
@@ -10,161 +10,160 @@ const DashboardCards = ({ monthexpiry }) => {
     year: "numeric",
   });
 
-  const cardsData = [
-    {
-      link: `/${role}/planexpirymonth`,
-      bgClass: "bg-gradient-moonlit",
-      value1:
-        monthexpiry?.monthexpiry?.length > 0
-          ? monthexpiry?.monthexpiry?.some(
+ const cardsData = [
+  {
+    link: `/${role}/planexpirymonth`,
+    bgClass: "bg-gradient-moonlit",
+    value1:
+      monthexpiry?.monthexpiry?.length > 0
+        ? monthexpiry?.monthexpiry?.some(
             (item) => fDateMonth(item?.month) === currentMonthYear
           )
-            ? monthexpiry?.monthexpiry.reduce((acc, item) => {
+          ? monthexpiry?.monthexpiry.reduce((acc, item) => {
               return fDateMonth(item?.month) === currentMonthYear
                 ? acc + (item.noofclient || 0)
                 : acc;
             }, 0)
-            : 0
-          : 0,
+          : 0
+        : 0,
+    label: "Current Month Active License",
+    icon: "bx bxs-id-card",
+    progress: 55,
+  },
+  {
+    link: `/${role}/client`,
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.clientCountTotal,
+    label: "Total Clients",
+    icon: "bx bxs-group",
+    progress: 55,
+  },
+  {
+    link: `/${role}/client`,
+    state: { clientStatus: 1 },
+    bgClass: "bg-gradient-ohhappiness",
+    value1: monthexpiry?.data?.clientCountActive,
+    label: "Total Active Clients",
+    icon: "bx bxs-user-check",
+    progress: 55,
+  },
+  {
+    link: `/${role}/client`,
+    state: { clientStatus: 0 },
+    bgClass: "bg-gradient-ibiza",
+    value1:
+      monthexpiry?.data.clientCountTotal - monthexpiry?.data.clientCountActive,
+    label: "Total Deactive Clients",
+    icon: "bx bxs-user-minus",
+    progress: 55,
+  },
+  {
+    link: `/${role}/signal`,
+    state: { clientStatus: "todayopensignal" },
+    bgClass: "bg-gradient-moonlit",
+    value1: monthexpiry?.data?.todayOpenSignal,
+    label: "Today's Open Signal",
+    icon: "bx bx-wifi",
+    progress: 55,
+  },
+  {
+    link: `/${role}/closesignal`,
+    state: { clientStatus: "todayclosesignal" },
+    bgClass: "bg-gradient-ibiza",
+    value1: monthexpiry?.data?.todayCloseSignal,
+    label: "Today's Close Signal",
+    icon: "bx bx-wifi-off",
+    progress: 55,
+  },
+  {
+    link: `/${role}/signal`,
+    bgClass: "bg-gradient-ohhappiness",
+    value1: monthexpiry?.data?.OpensignalCountTotal,
+    label: "Total Open Signals",
+    icon: "bx bx-broadcast",
+    progress: 55,
+  },
+  {
+    link: `/${role}/closesignal`,
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.CloseSignalCountTotal,
+    label: "Total Close Signals",
+    icon: "bx bx-no-signal",
+    progress: 55,
+  },
+  {
+    link: `/${role}/client`,
+    state: { clientStatus: "active" },
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.activePlanclient,
+    label: "Total Plan Active Clients",
+    icon: "bx bx-signal-5",
+    progress: 55,
+  },
+  {
+    link: `/${role}/client`,
+    state: { clientStatus: "expired" },
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.inActivePlanclient,
+    label: "Total Plan Expired ",
+    icon: "bx bx-timer",
+    progress: 55,
+  },
+  {
+    link: `/${role}/freeclient`,
+    state: { clientStatus: "active" },
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.activeFreetrial,
+    label: "Total Active Free Clients",
+    icon: "bx bx-user",
+    progress: 55,
+  },
+  {
+    link: `/${role}/freeclient`,
+    state: { clientStatus: "expired" },
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.inActiveFreetrial,
+    label: "Total Inactive Free Clients",
+    icon: "bx bx-user-x",
+    progress: 55,
+  },
+  {
+    link: `/${role}/todayExpiry-data`,
+    state: { clientStatus: 0 },
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.counts?.["0"],
+    label: "Today's Expiry Data",
+    icon: "bx bx-calendar-exclamation",
+    progress: 55,
+  },
+  {
+    link: `/${role}/yesterdayExpiry-data`,
+    state: { clientStatus: -1 },
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.counts?.["-1"],
+    label: "Yesterday Expiry Data",
+    icon: "bx bx-calendar-minus",
+    progress: 55,
+  },
+  {
+    link: `/${role}/tomorrowExpiry-data`,
+    state: { clientStatus: 1 },
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.counts?.["1"],
+    label: "Tomorrow Expiry Data",
+    icon: "bx bx-calendar-event",
+    progress: 55,
+  },
+  {
+    link: `/${role}/CurrentMonth-freeTrial`,
+    bgClass: "bg-gradient-deepblue",
+    value1: monthexpiry?.data?.totalfreetrial,
+    label: "Current Month Free Trial",
+    icon: "bx bxs-gift",
+    progress: 55,
+  },
+];
 
-      label: "Current Month Active License",
-      icon: "bx-user-plus",
-      progress: 55,
-    },
-    {
-      link: `/${role}/client`,
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.clientCountTotal,
-      label: "Total Clients",
-      icon: "bx-user",
-      progress: 55,
-    },
-    {
-      link: `/${role}/client`,
-      state: { clientStatus: 1 },
-      bgClass: "bg-gradient-ohhappiness",
-      value1: monthexpiry?.data?.clientCountActive,
-      label: "Total Active Clients",
-      icon: "bx-user-circle",
-      progress: 55,
-    },
-    {
-      link: `/${role}/client`,
-      state: { clientStatus: 0 },
-      bgClass: "bg-gradient-ibiza",
-      value1:
-        monthexpiry?.data.clientCountTotal -
-        monthexpiry?.data.clientCountActive,
-      label: "Total Deactive Clients",
-      icon: "bx-user-x",
-      progress: 55,
-    },
-    {
-      link: `/${role}/signal`,
-      state: { clientStatus: "todayopensignal" },
-      bgClass: "bg-gradient-moonlit",
-      value1: monthexpiry?.data?.todayOpenSignal,
-      label: "Today's Open Signal",
-      icon: "bx-wifi-2",
-      progress: 55,
-    },
-    {
-      link: `/${role}/closesignal`,
-      state: { clientStatus: "todayclosesignal" },
-      bgClass: "bg-gradient-ibiza",
-      value1: monthexpiry?.data?.todayCloseSignal,
-      label: "Today's Close Signal",
-      icon: "bx-wifi-off",
-      progress: 55,
-    },
-    {
-      link: `/${role}/signal`,
-      bgClass: "bg-gradient-ohhappiness",
-      value1: monthexpiry?.data?.OpensignalCountTotal,
-      label: "Total Open Signals",
-      icon: "bxl-redux",
-      progress: 55,
-    },
-    {
-      link: `/${role}/closesignal`,
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.CloseSignalCountTotal,
-      label: "Total Close Signals",
-      icon: "bx-wifi-2",
-      progress: 55,
-    },
-    {
-      link: `/${role}/client`,
-      state: { clientStatus: "active" },
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.activePlanclient,
-      label: "Total Plan Active Clients",
-      icon: "bx-wifi-2",
-      progress: 55,
-    },
-    {
-      link: `/${role}/client`,
-      state: { clientStatus: "expired" },
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.inActivePlanclient,
-      label: "Total Plan Expired ",
-      icon: "bx bx-wifi-2 fs-3",
-      progress: 55,
-    },
-    {
-      link: `/${role}/freeclient`,
-      state: { clientStatus: "active" },
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.activeFreetrial,
-      label: "Total Active Free Clients",
-      icon: "bx bx-wifi-2 fs-3",
-      progress: 55,
-    },
-    {
-      link: `/${role}/freeclient`,
-      state: { clientStatus: "expired" },
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.inActiveFreetrial,
-      label: "Total Inactive Free Clients",
-      icon: "bx bx-wifi-2 fs-3",
-      progress: 55,
-    },
-    {
-      link: `/${role}/todayExpiry-data`,
-      state: { clientStatus: 0 },
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.counts?.["0"],
-      label: "Today's Expiry Data",
-      icon: "bx bx-wifi-2 fs-3",
-      progress: 55,
-    },
-    {
-      link: `/${role}/yesterdayExpiry-data`,
-      state: { clientStatus: -1 },
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.counts?.["-1"],
-      label: "Yesterday Expiry Data",
-      icon: "bx bx-wifi-2 fs-3",
-      progress: 55,
-    },
-    {
-      link: `/${role}/tomorrowExpiry-data`,
-      state: { clientStatus: 1 },
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.counts?.["1"],
-      label: "Tomorrow Expiry Data",
-      icon: "bx bx-wifi-2 fs-3",
-      progress: 55,
-    },
-    {
-      link: `/${role}/CurrentMonth-freeTrial`,
-      bgClass: "bg-gradient-deepblue",
-      value1: monthexpiry?.data?.totalfreetrial,
-      label: "Current Month Free Trial",
-      icon: "bx bx-wifi-2 fs-3",
-      progress: 55,
-    },
-  ];
 
   return (
     <div className="row newbg dashboard-card">
